c9be59025e2fdff72674d304d5662a511976b6f8
Ariel Calderón <khaldera@hotmail.com>
2025-04-06 20:05:52 -0300

    Agregado de funciones para chequear los campos obligatorios

--- vista.py
+++ vista.py
@@ -1,6 +1,7 @@
 from cProfile import label
+from pickle import FALSE
 import tkinter as tk
-from tkinter import Menu, ttk
+from tkinter import TRUE, Menu, ttk, messagebox
 from logica import Producto, Seccion, ListaSecciones
 
 from setuptools import Command
@@ -103,20 +104,21 @@ class Plantilla(tk.Toplevel):
     def __init__(self,parent,clase_objeto):
         super().__init__(parent)
         self.clase_objeto = clase_objeto
-        self.lista_de_comboboxes = [] # [[objeto combox,"campo clave externa db",lista de id],]
+        self.lista_de_comboboxes = [] # [[objeto combox,"campo clave externa db",lista de id],] Sin incluir la clave principal
         self.clave_principal = [] #[objeto combobox, lista de id] cuando está implementada con un combobox
                                   #[objeto entry] cuando se trata de una entrada de texto y el valor se obtiene con get
+        self.campos_obligatorios=[] #[campo de entrada] tiene que ser el objeto del input, sea un combobox o un entry simple 
     
     def guardar(self):
-        self.objeto = self.clase_objeto()        
-        self.objeto.asignar_valores(self)
-        self.objeto.guardar()
+        if self.chequear_campos_obligatorios():
+            self.objeto = self.clase_objeto()        
+            self.objeto.asignar_valores(self)
+            self.objeto.guardar()
 
     def modificar(self):
-        self.objeto.asignar_valores(self)
-        self.objeto.modificar()
-
-   
+        if self.chequear_campos_obligatorios():
+            self.objeto.asignar_valores(self)
+            self.objeto.modificar()
     
     def seleccionar(self):   
         if (len(self.clave_principal)>1):
@@ -127,8 +129,52 @@ class Plantilla(tk.Toplevel):
         
         self.objeto = self.clase_objeto(id)
         self.objeto.completar_campos(self)
+        self.switch_widgets()
 
-        
+    def switch_widgets(self):
+        for widget in self.winfo_children():
+            try:
+                estado_actual = str(widget.cget("state"))
+                if isinstance(widget,ttk.Combobox):
+                    nuevo_estado = "readonly" if estado_actual == "disabled" else "disabled"
+                else:
+                    nuevo_estado = "normal" if estado_actual == "disabled" else "disabled"             
+                widget.configure(state=nuevo_estado)
+            except (tk.TclError, AttributeError):
+                pass  
+
+    def chequear_campos_obligatorios(self):  
+        mensaje = ""  
+        check = True 
+        print (self.campos_obligatorios)     
+        for campo in self.campos_obligatorios:
+            variable = campo.cget("textvariable")
+            valor = self.getvar(variable)
+            print (valor)
+            if valor is None or valor == "":                
+                mensaje += f"El campo {self.obtener_label(campo)} está vacío\n"
+        if mensaje != "":
+            check = False 
+            messagebox.showinfo("Falta información", mensaje)
+            print (mensaje)                   
+        return check
+
+
+    def obtener_label(self, widget_objetivo):
+        info = widget_objetivo.grid_info()
+        fila = int(info["row"])
+        columna = int(info["column"])
+        contenedor = widget_objetivo.master
+
+        for widget in contenedor.winfo_children():
+            if isinstance(widget, tk.Label):
+                info_label = widget.grid_info()
+                if int(info_label["row"]) == fila and int(info_label["column"]) == columna -1:
+                    return widget.cget("text")
+        return None
+
+
+   
                 
                 
 
@@ -148,14 +194,17 @@ class PlantillaProducto(Plantilla):
         #self.nombre_de_seccion = tk.StringVar()
         secciones = ListaSecciones()
         lista_de_secciones_descripcion = secciones.listar_columnas(["descripcion"])
-        lista_de_secciones_id = secciones.listar_columnas(["id"])
-        self.seccion_entrada = ttk.Combobox(self,values=lista_de_secciones_descripcion)
-        self.seccion_entrada.grid(row=0, column=1, padx=10, pady=5)
+        lista_de_secciones_id = secciones.listar_columnas(["id"])  
+        self.seccion_variable = tk.StringVar()
+        self.seccion_entrada = ttk.Combobox(self,textvariable=self.seccion_variable,values=lista_de_secciones_descripcion,state="readonly")
+        self.seccion_entrada.grid(row=0, column=1, padx=10, pady=5)      
 
         #declaro el combobox y lo agrego a la lista, para que pueda ser procesado
         #en las funciones de guardar, seleccionar y modificar.
         combo_1= [self.seccion_entrada,"secciones",lista_de_secciones_id]
-        self.lista_de_comboboxes.append(combo_1)
+        self.lista_de_comboboxes.append(combo_1)        
+        self.campos_obligatorios.append(self.seccion_entrada)
+
 
         tk.Label(self, text="Código de PLU").grid(row=1, column=0, padx=10, pady=5, sticky="w")
         self.codigo_de_PLU = tk.StringVar()
@@ -163,20 +212,25 @@ class PlantillaProducto(Plantilla):
         self.codigo_de_PLU_entrada.grid(row=1, column=1, padx=10, pady=5)
 
         self.clave_principal = [self.codigo_de_PLU]
+        self.campos_obligatorios.append(self.codigo_de_PLU_entrada)
 
-        if (modo=="modificar"):            
-            tk.Button(self, text="Seleccionar", command= self.seleccionar).grid(row=1, column=2, columnspan=2, pady=20)
+        if (modo=="modificar"): 
+            self.codigo_de_PLU_entrada.configure(state="disabled")           
+            tk.Button(self, text="Seleccionar", command= self.seleccionar,state="disabled").grid(row=1, column=2, columnspan=2, pady=20)
 
         tk.Label(self, text="Descripción").grid(row=2, column=0, padx=10, pady=5, sticky="w")
         self.descripcion = tk.StringVar()
         self.descripcion_entrada = tk.Entry(self,textvariable=self.descripcion)
         self.descripcion_entrada.grid(row=2, column=1, padx=10, pady=5)
 
+        self.campos_obligatorios.append(self.descripcion_entrada)
+
 
         if(modo=="guardar"):            
             tk.Button(self, text="Guardar", command=self.guardar).grid(row=3, column=0, columnspan=2, pady=20)
         else:
             tk.Button(self, text="Modificar", command=self.modificar).grid(row=3, column=0, columnspan=2, pady=20)
+            self.switch_widgets()
 
    
 
@@ -194,7 +248,7 @@ class PlantillaSeccion(Plantilla):
             lista_de_secciones_descripcion = secciones.listar_columnas(["descripcion"])
             
 
-            tk.Label(self, text="Sección: ").grid(row=0, column=0, padx=10, pady=5, sticky="w")
+            tk.Label(self, text="Sección ").grid(row=0, column=0, padx=10, pady=5, sticky="w")
             #no es necesario usar tk.StringVar() ya que el id se obtiene de la lista
             self.id_entrada = ttk.Combobox(self,  values=lista_de_secciones_descripcion)
             self.id_entrada.grid(row=0, column=1, padx=10, pady=5)
